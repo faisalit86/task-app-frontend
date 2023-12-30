bf04c7fdc9ef85835bf5697e7d79b1b1ab457f27
ccriptdev8 <mfaisalit86@gmail.com>
2023-12-30 12:32:55 -0800

    setup frontend

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+        <h2 className="">Test task</h2>
+    </div>
+  );
+}
+
+export default App;
